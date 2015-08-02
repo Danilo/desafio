afedff6bdb70cb75464db1020b2f03ab7e7e2cf3
danilo <danilo.maldito@gmail.com>
2015-08-02 13:46:24 -0300

    Added test for insert operation.

--- tests/test_desafio.py
+++ tests/test_desafio.py
@@ -1,6 +1,14 @@
-import unittest
+import unittest, urllib2
 
 class TestFunctions(unittest.TestCase):
+    def test_insert(self):
+        url = 'http://127.0.0.1:5000/person/'
+        data = 'facebookId=670286562'
+        request = urllib2.Request(url, data)
+        content = urllib2.urlopen(request)
+        for x in content:
+            self.assertEqual(x, 'HTTP 201')
+        content.close()
 
 if __name__ == '__main__':
     unittest.main()
